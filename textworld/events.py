1fc67b3dbea706e25a92b888132dcd6d1d069dc6
Tyler Evans <tyler@poorlythoughtout.com>
2025-02-21 13:30:56 -0800

    new events

--- textworld/events.py
+++ textworld/events.py
@@ -2,11 +2,18 @@ from relative_world.event import Event
 
 SAID_ALOUD_EVENT_TYPE = "SAID_ALOUD"
 PERFORMED_ACTION_EVENT_TYPE = "PERFORMED_ACTION"
-
+THOUGHT_EVENT_TYPE = "THOUGHT"
 
 class SaidAloudEvent(Event):
     type: str = SAID_ALOUD_EVENT_TYPE
+    message: str
 
 
 class PerformedActionEvent(Event):
     type: str = PERFORMED_ACTION_EVENT_TYPE
+    action: str
+
+
+class ThoughtEvent(Event):
+    type: str = THOUGHT_EVENT_TYPE
+    thought: str
