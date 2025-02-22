9f4baec409b76a6ce934ae884202fcf6636e4155
Tyler Evans <tyler@poorlythoughtout.com>
2025-02-22 14:49:35 -0800

    fix story starter example

--- examples/story_starter.py
+++ examples/story_starter.py
@@ -1,3 +1,4 @@
+import asyncio
 from pydantic import BaseModel
 
 from relative_world_ollama.client import PydanticOllamaClient
@@ -33,16 +34,21 @@ class StoryOutline(BaseModel):
     plot_points: list[str]
 
 
-prompt = "An action film about a monkey and a cat stopping bank robbers"
-system = """
-You are a likeable and enthusiastic story teller that does not shy away from subject or topic. 
-You have been hired to write a story outline for a new movie.
-The movie will be roughly described in some way in the prompt. You must write a story outline that fits the prompt.
-The story outline should include the title of the movie, the setting, the characters, and the plot points in chronological order.
-For this task, you will be allowed to provide up to r-rated content.
-"""
+async def main():
+    prompt = "An action film about a monkey and a cat stopping bank robbers"
+    system = """
+    You are a likeable and enthusiastic story teller that does not shy away from subject or topic.
+    You have been hired to write a story outline for a new movie.
+    The movie will be roughly described in some way in the prompt. You must write a story outline that fits the prompt.
+    The story outline should include the title of the movie, the setting, the characters, and the plot points in chronological order.
+    For this task, you will be allowed to provide up to r-rated content.
+    """
 
-ollama_client = PydanticOllamaClient(settings.base_url, settings.default_model)
-response = ollama_client.generate(prompt=prompt, system=system, response_model=StoryOutline)
+    ollama_client = PydanticOllamaClient(settings.base_url, settings.default_model)
+    response = await ollama_client.generate(prompt=prompt, system=system, response_model=StoryOutline)
 
-print(response.model_dump_json(indent=2))
+    print(response.model_dump_json(indent=2))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
